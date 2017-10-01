3c9e056a54099241324ff136c32247adc985ae6c
kkkfff <32432719+kkkfff@users.noreply.github.com>
2017-10-01 15:31:22 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+string name;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout <<  "Hello, what's your name? ...";
+    cin >> name;
+    cout << "Oh, nice to meet you, "<<name<<"!"<<endl;
+    return 0;
+}
