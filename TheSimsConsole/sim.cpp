0d9be19fb9e08ccb3a205e16c073710f6017af40
kkkfff <32432719+kkkfff@users.noreply.github.com>
2017-10-28 14:57:51 +0200

    Add files via upload

--- TheSimsConsole/sim.cpp
+++ TheSimsConsole/sim.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "sim.h"
+#include <iomanip>
+#include <windows.h>
 
 using namespace std;
 
@@ -78,15 +80,34 @@ void Sim::show_me()
 
 void Sim::show_status()
 {
+	system("cls");
+    HANDLE hOut = GetStdHandle( STD_OUTPUT_HANDLE );
+    SetConsoleTextAttribute( hOut, FOREGROUND_RED | FOREGROUND_GREEN );
 
-    cout<<"Energy [1-10]: "<<energy<<endl;
+    cout<<fixed<<right;
+        cout<<endl<<" \xDB  Active Sim: "<<name<<" "<<surname<<endl;
+    cout<<"\xC9";
+    for(int i=0; i<52; i++) cout<<"\xCD"; cout<<"\xBB";
+
+    cout<<endl<<"\xBA"<<setw(10)<<"Energy: "; for(int i=0;i<energy;i++) cout<<"\xB2"; for(int j=0;j<(10-energy);j++) cout<<"\xB0";
+        cout<<setw(20)<<"Bladder: "; for(int i=0;i<bladder;i++) cout<<"\xB2"; for(int j=0;j<(10-bladder);j++) cout<<"\xB0"; cout<<"  \xBA";
+    cout<<endl<<"\xBA"<<setw(10)<<"Hunger: "; for(int i=0;i<hunger;i++) cout<<"\xB2"; for(int j=0;j<(10-hunger);j++) cout<<"\xB0";
+        cout<<setw(20)<<"Hygiene: "; for(int i=0;i<hygiene;i++) cout<<"\xB2"; for(int j=0;j<(10-hygiene);j++) cout<<"\xB0"; cout<<"  \xBA";
+    cout<<endl<<"\xBA"<<setw(10)<<"Fun: ";for(int i=0;i<fun;i++)        cout<<"\xB2"; for(int j=0;j<(10-fun);j++) cout<<"\xB0";
+        cout<<setw(20)<<"Environment: "; for(int i=0;i<environment;i++)cout<<"\xB2"; for(int j=0;j<(10-environment);j++) cout<<"\xB0"; cout<<"  \xBA";
+    cout<<endl<<"\xBA"<<setw(10)<<"Social: ";for(int i=0;i<social;i++)  cout<<"\xB2"; for(int j=0;j<(10-social);j++) cout<<"\xB0";
+        cout<<setw(20)<<"Comfort: "; for(int i=0;i<comfort;i++)   cout<<"\xB2"; for(int j=0;j<(10-comfort);j++) cout<<"\xB0"; cout<<"  \xBA";
+    cout<<endl<<"\xC8";
+    for(int i=0; i<52; i++) cout<<"\xCD"; cout<<"\xBC";
+    SetConsoleTextAttribute( hOut, FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_GREEN );
+    /*cout<<"Energy [1-10]: "<<energy<<endl;
     cout<<"Hunger [1-10]: "<<hunger<<endl;
     cout<<"Fun [1-10]: "<<fun<<endl;
     cout<<"Social [1-10]: "<<social<<endl;
     cout<<"Bladder [1-10]: "<<bladder<<endl;
     cout<<"Hygiene [1-10]: "<<hygiene<<endl;
     cout<<"Environment [1-10]: "<<environment<<endl;
-    cout<<"Comfort [1-10]: "<<comfort<<endl;
+    cout<<"Comfort [1-10]: "<<comfort<<endl;*/
 }
 
 Simon::Simon(bool s, string n, string sn, int a, int ne, int sh, int ac, int pu, int ene, int hun, int fu, int soc, int bla, int hyg, int env, int com):Sim(s,n,sn,a,ne,sh,ac,pu,ene,hun,fu,soc,bla,hyg,env,com)
