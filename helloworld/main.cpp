9ca1bf6590c81635b2dfc8dc0cb4c7a2ab226a88
kaeF <fuder@poczta.fm>
2018-05-19 14:48:52 +0200

    ..::testowy pierwszy na poczatek

--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -8,6 +8,6 @@ int main()
     cout << "Hello world!" << endl;
     cout <<  "Hello, what's your name? ...";
     cin >> name;
-    cout << "Oh, nice to meet you, "<<name<<"!"<<endl;
+    cout << "Oh, It's not nice to meet you. "<<name<<"!"<<endl;
     return 0;
 }
